c71f1aa5ce56a3b6d71986d1b590bb9acd1adc20
amandasawyerr <114168681+amandasawyerr@users.noreply.github.com>
2022-12-04 21:34:24 -0500

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,1 +1,11 @@
+// Adapted from w3schools
+// Changes colour of title when mouse passes over 
 
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1 onmouseover="style.color='red'" onmouseout="style.color='black'">SQjr</h1>
+
+</body>
+</html>
