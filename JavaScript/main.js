582d88f1e6e32d558e546f113467978844eaa3d0
amandasawyerr <114168681+amandasawyerr@users.noreply.github.com>
2022-12-05 20:37:14 -0500

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,11 +1,17 @@
-// Adapted from w3schools
-// Changes colour of title when mouse passes over 
+// By Amanda 
 
 <!DOCTYPE html>
 <html>
 <body>
-
+  
+// Adapted from w3schools
+// Changes colour of title when mouse passes over 
 <h1 onmouseover="style.color='red'" onmouseout="style.color='black'">SQjr</h1>
+
+// Adapted from w3schools
+// Changes html content upon event / clicking of button
+<p id="demo">Click the button below to contact us.</p>
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Just kidding"'>Click Me!</button>
 
 </body>
 </html>
